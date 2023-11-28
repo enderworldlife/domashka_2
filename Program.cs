654356fe80e2958edaa4c32dda23e5c8776df0a3
Your Name <you@example.com>
2023-11-28 23:17:58 +0300

    Zadanye 3

--- Program.cs
+++ Program.cs
@@ -1,11 +1,48 @@
-﻿
+﻿// zadanye 1
+// Console.Write("Vvedite chsilo: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// bool numberdeleniyena7 = false;
+// bool numberdeleniyena23 = false;
+// if (number % 7 == 0) numberdeleniyena7 = true;
+// if (number % 23 == 0) numberdeleniyena23 = true;
+// if (numberdeleniyena7 == false && numberdeleniyena23 == false) Console.WriteLine("Chsilo ne kratno");
+// else if (numberdeleniyena7 == true) Console.WriteLine("Chislo kratno tolko na 7");
+// else if (numberdeleniyena23 == true) Console.WriteLine("Chislo kratno tolko na 23");
+// else if (numberdeleniyena7 == true && numberdeleniyena23 == true) Console.WriteLine("Chislo kratno na 7 i na 23");
+
+// zadanye 2
+
+// zadanye 3
+
+// Console.Write("Vvedite chsilo: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+using System.Security.Principal;
+
 Console.Write("Vvedite chsilo: ");
-int number = Convert.ToInt32(Console.ReadLine());
-bool numberdeleniyena7 = false;
-bool numberdeleniyena23 = false;
-if (number % 7 == 0) numberdeleniyena7 = true;
-if (number % 23 == 0) numberdeleniyena23 = true;
-if (numberdeleniyena7 == false && numberdeleniyena23 == false) Console.WriteLine("Chsilo ne kratno");
-else if (numberdeleniyena7 == true) Console.WriteLine("Chislo kratno tolko na 7");
-else if (numberdeleniyena23 == true) Console.WriteLine("Chislo kratno tolko na 23");
-else if (numberdeleniyena7 == true && numberdeleniyena23 == true) Console.WriteLine("Chislo kratno na 7 i na 23");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int n = num;
+
+while(n > 0){
+    
+    n /= 10;
+    i++;
+}
+Console.Write("Vvedeno chislo: ");
+Console.WriteLine(num);
+Console.Write("Kolichestvo tsifer: ");
+Console.WriteLine(i);
+Console.Write("Tsifry: ");
+
+int max = 0;
+while(i > 0){
+    int last = num % 10;
+    if (max < last) max = last;
+    num /= 10;
+    Console.Write(last + ", ");
+    i--;
+}
+Console.WriteLine(" ");
+Console.Write("Maksimalnaya tsifra: ");
+Console.WriteLine(max);
+
