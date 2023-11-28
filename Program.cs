1cf603307bd467513dcf558d71c032aa67c8f145
Your Name <you@example.com>
2023-11-28 17:46:39 +0300

    pervoye zadaniye

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿
+Console.Write("Vvedite chsilo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool numberdeleniyena7 = false;
+bool numberdeleniyena23 = false;
+if (number % 7 == 0) numberdeleniyena7 = true;
+if (number % 23 == 0) numberdeleniyena23 = true;
+if (numberdeleniyena7 == false && numberdeleniyena23 == false) Console.WriteLine("Chsilo ne kratno");
+else if (numberdeleniyena7 == true) Console.WriteLine("Chislo kratno tolko na 7");
+else if (numberdeleniyena23 == true) Console.WriteLine("Chislo kratno tolko na 23");
+else if (numberdeleniyena7 == true && numberdeleniyena23 == true) Console.WriteLine("Chislo kratno na 7 i na 23");
