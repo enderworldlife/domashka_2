ffc896ab1a54d0ba26be7be03160d461f120bf01
Your Name <you@example.com>
2023-11-28 23:48:59 +0300

    zadanye 4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// zadanye 1
+
 // Console.Write("Vvedite chsilo: ");
 // int number = Convert.ToInt32(Console.ReadLine());
 // bool numberdeleniyena7 = false;
@@ -12,37 +13,56 @@
 
 // zadanye 2
 
+// Простите пожалуйста. Я не смог решить эту задачу(
+
 // zadanye 3
 
 // Console.Write("Vvedite chsilo: ");
 // int number = Convert.ToInt32(Console.ReadLine());
+using System.Diagnostics;
+using System.Globalization;
 using System.Security.Principal;
+
+// Console.Write("Vvedite chsilo: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 0;
+// int n = num;
+
+// while(n > 0){
+    
+//     n /= 10;
+//     i++;
+// }
+// Console.Write("Vvedeno chislo: ");
+// Console.WriteLine(num);
+// Console.Write("Kolichestvo tsifer: ");
+// Console.WriteLine(i);
+// Console.Write("Tsifry: ");
+
+// int max = 0;
+// while(i > 0){
+//     int last = num % 10;
+//     if (max < last) max = last;
+//     num /= 10;
+//     Console.Write(last + ", ");
+//     i--;
+// }
+// Console.WriteLine(" ");
+// Console.Write("Maksimalnaya tsifra: ");
+// Console.WriteLine(max);
+
+// zadanye 4
 
 Console.Write("Vvedite chsilo: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int i = 0;
-int n = num;
 
-while(n > 0){
-    
-    n /= 10;
-    i++;
-}
-Console.Write("Vvedeno chislo: ");
-Console.WriteLine(num);
-Console.Write("Kolichestvo tsifer: ");
-Console.WriteLine(i);
-Console.Write("Tsifry: ");
-
-int max = 0;
-while(i > 0){
+string print = string.Empty;
+while(num > 0){
     int last = num % 10;
-    if (max < last) max = last;
     num /= 10;
-    Console.Write(last + ", ");
-    i--;
+    print = last + ", " + print;
 }
-Console.WriteLine(" ");
-Console.Write("Maksimalnaya tsifra: ");
-Console.WriteLine(max);
+Console.WriteLine(print);
+
+
 
